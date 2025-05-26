146ade87aeeeb902cc8cbaa59f1d6cfbf6de0fb4
Istiaq Alam <168295675+Istiaq-Alam@users.noreply.github.com>
2025-05-26 13:29:54 +0600

    FCFS updated

--- Algorithms/FCFS.cpp
+++ Algorithms/FCFS.cpp
@@ -6,7 +6,6 @@ int main()
     //cout << "Enter Number of Process : ";
     int n=3;
     //cin >> n;
-
     int i, j, CT[n], TAT[n], WT[n];
     int PS[]={1,2,3};
     int AT[]={1,2,3};
@@ -25,18 +24,15 @@ int main()
         {
             if(AT[j] > AT[j+1])
             {
-
                 swap(AT[j],AT[j+1]);
             }
         }
     }
-
     CT[0]=AT[0]+BT[0];
     for(i=1; i<n; i++)
     {
         CT[i]=CT[i-1]+BT[i];
     }
-
     int Total_T, Total_W;
     for(i=0; i<n; i++)
     {
@@ -46,14 +42,21 @@ int main()
         Total_T=Total_T+TAT[i];
         Total_W=Total_W+WT[i];
     }
-
-    cout << "Process\tArrival time\tBurst time\tCompletition time\tTurnaround time\tWaiting time"<<endl;
+    cout << "AT - Arrival Time" << endl;
+    cout << "BT - Brust Time" << endl;
+    cout << "CT - Completition Time" << endl;
+    cout << "TT - Turnaround Time" << endl;
+    cout << "WT - Waiting Time" << endl<<endl;
+    cout << "-------------------------------------------------"<<endl;
+    cout << "|Process |AT\t|BT\t|CT\t|TT\t|WT\t|"<<endl;
+    cout << "-------------------------------------------------"<<endl;
 
     for (i=0; i<n; i++)
     {
-        cout << "P"<<PS[i]<<"\t"<<AT[i]<<"\t"<<BT[i]<<"\t"<<CT[i]<<"\t"<<TAT[i]<<"\t"<<WT[i]<<endl;
+        cout << "|P"<<PS[i]<<"\t |"<<AT[i]<<"\t|"<<BT[i]<<"\t|"
+        <<CT[i]<<"\t|"<<TAT[i]<<"\t|"<<WT[i]<<"\t|"<<endl;
     }
-
+    cout << "-------------------------------------------------"<<endl;
     cout << "Average Turnaround Time : " <<Total_T/n<<endl;
     cout << "Average Waiting Time : " <<Total_W/n<<endl;
 }
